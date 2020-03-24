99cd82ae304d661f945d194c334d90cf608bc479
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2020-03-24 12:23:21 -0700

    single step inside memory instr until breakpoints are hit. Continue executions has issues with radare2

--- EMU-Scripts/Unpacker-r2.py
+++ EMU-Scripts/Unpacker-r2.py
@@ -4,30 +4,46 @@ import r2pipe
 import json
 
 r = r2pipe.open(
-    filename="cryptowall.bin", flags=['-b 32']
+    filename="cryptowall.bin"
 )
 
 def get_eip():
     return json.loads(r.cmd('drj'))['rip']
 
 r.cmd('aaaa')
-r.cmd('e asm.bits=32')
 r.cmd('doo')
 r.cmd('db main')
 r.cmd('doo')
 r.cmd('dc')
 r.cmd('dc')
 r.cmd('dc') # should be in main now
-print('Current addr: ' + hex(get_eip()))
+print('In main: ' + hex(get_eip()))
 
 r.cmd('db 0x00402dda') # breakpoint at the end of push eax
 r.cmd('dc')
 print(r.cmd('ds 2')) # step into eax
 print(r.cmd('pd 10')) # now we are inside the stage 2 of unpacking
 
+print("Hold your horses... this may take awhile")
+r.cmd('dsu 0x0302CA57')
+print('Inside Second stage loader: ' + hex(get_eip()))
+r.cmd('ds 1') #step into EAX
+print('Inside Third stage loader: ' + hex(get_eip()))
+r.cmd('dsu 0x00191446')
+r.cmd('dr al=0xC0')
+r.cmd('ds 2')
+print('Patched Third stage loader: ' + hex(get_eip()))
+
 VirtualAlloc = r.cmd('? [sym.imp.KERNEL32.dll_VirtualAlloc]')
 print('VirtualAlloc is at: ' + VirtualAlloc)
-r.cmd('db [sym.imp.KERNEL32.dll_VirtualAlloc]')
-print('Current addr: ' + hex(get_eip()))
-r.cmd('dc')
-print('Current after stepping into VirtualAlloc: ' + hex(get_eip()))
+
+r.cmd('dsu [sym.imp.KERNEL32.dll_VirtualAlloc]')
+print('Inside VirtualAlloc Part I: ' + hex(get_eip()))
+r.cmd('ds 4')
+r.cmd('dsu [sym.imp.KERNEL32.dll_VirtualAlloc]')
+r.cmd('ds 4')
+r.cmd('dsu [sym.imp.KERNEL32.dll_WriteProcessMemory]')
+r.cmd('ds 4')
+r.cmd('dsu [sym.imp.KERNEL32.dll_WriteProcessMemory]')
+print("Found dumped PE")
+print(r.cmd('px @ ecx'))
