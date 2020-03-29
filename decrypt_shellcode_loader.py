4440fa4a5dd5e6534125e12742faaba49b1b7f96
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2020-03-29 10:06:42 -0700

    decrypted resource section to x86 shellcode

--- decrypt_shellcode_loader.py
+++ decrypt_shellcode_loader.py
@@ -1,5 +1,6 @@
 import pefile
 import base64
+from capstone import *
 
 
 # the resource section contains the 4th stage loader which injects a new PE file
@@ -24,7 +25,7 @@ def CustomBase64Decode(b64_str):
     next_char = 0
     i, y, n, z = 0, 0, 0, 0
 
-    # len(b64_str) + (-0x9a8)
+    # len(b64_str) + (-0x9a8), size used at VirtualAlloc
     for _ in range(0x15CB4):
         char = 1
         while(1):
@@ -51,12 +52,50 @@ def CustomBase64Decode(b64_str):
     return buffer
 
 
-def DecryptToShellCode(b64decoded_str):
-    # TODO
-    return
+def DecryptToShellCode(b64decoded_str, size_of):
+    tmp_buffer = bytearray()
+    for _ in range(size_of):
+        tmp_buffer += bytes([0])
+
+    keys = [0x65, 0x00, 0x00, 0x00, 0xD6, 0x00, 0x00, 0x00, 0x0A]
+    n = 1
+    idx = 0
+    tmp_char_1 = 0
+    tmp_char_2 = 0
+    tmp_char_3 = 0
+
+    for _ in range(size_of):
+        tmp_buffer[idx] = b64decoded_str[idx]
+        tmp_char_1 = (tmp_buffer[idx] - (keys[8] ^ n) & 0xff) # convert negative value into unsigned int
+        tmp_char_2 = (tmp_char_1 - (keys[4] ^ n) & 0xff)
+        tmp_char_3 = (tmp_char_2 - (keys[0] ^ n) & 0xff)
+        tmp_buffer[idx] = tmp_char_3
+
+        idx += 1
+        n += 1
+
+    return tmp_buffer
+
+
+def printHexVal(arr, size):
+    for i in range(size):
+        if i % 16 == 0:
+            print("")
+        print("{} ".format(hex(arr[i])), end='')
+    print("")
+
+
+def saveDissassemblyFromBytes(shellcode, filename):
+    file = open(filename, 'w')
+    md = Cs(CS_ARCH_X86, CS_MODE_32)
+    for i in md.disasm(shellcode, 0x1000):
+        file.write("0x%x:\t%s\t%s\n" % (i.address, i.mnemonic, i.op_str))
+    file.close()
 
 
 def main():
+    output_filepath = "extractions/pe_process_injector_dump.asm"
+
     b64_str = GetEncryptedShellcode("cryptowall.bin", 0xa0, 0x1665c)
     if b64_str[0:2] == 'cy':
         print("\n[+] Successfully extracted encoded shellcode")
@@ -64,12 +103,16 @@ def main():
     b64_decoded = CustomBase64Decode(b64_str)
     if b64_decoded[0:2] == b'\x9b\xce':
         print("[+] Successfully decoded encrypted shellcode")
+    #Verify the bytes match in the programs debugger
+    printHexVal(b64_decoded, 64)
 
-    # Verify the bytes match in the programs debugger
-    for i in range(64):
-        if i % 16 == 0:
-            print("")
-        print("{} ".format(hex(b64_decoded[i])), end='')
+    decrypted_shellcode = DecryptToShellCode(b64_decoded, len(b64_decoded))
+    if decrypted_shellcode[0:2] == b'\x55\x8b':
+        print("\n\n[+] Successfully decrypted shellcode")
+        printHexVal(decrypted_shellcode, 64)
+        print("\n[+] Using Capstone to Disassemble shellcode to x86")
+        saveDissassemblyFromBytes(decrypted_shellcode, output_filepath)
+        print("[+] Successfully saved assembly dump file to {}".format(output_filepath))
 
 
 if __name__ == '__main__':
