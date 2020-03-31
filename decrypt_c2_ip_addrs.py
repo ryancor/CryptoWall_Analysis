f27cd75cd61d76ae07afbce6855dab9871f8d3c5
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2020-03-31 10:44:47 -0700

    algorithms identified as RC4

--- decrypt_c2_ip_addrs.py
+++ decrypt_c2_ip_addrs.py
@@ -1,7 +1,8 @@
 import pefile
 
+RC4_Keylen = 256
 
-def getEncryptedBytes(filename, offset, size):
+def GetEncryptedBytes(filename, offset, size):
     pe = pefile.PE(filename)
     enc_arr = []
 
@@ -20,17 +21,18 @@ def GrabPlaintextPassword(string_buffer):
     return ''.join([chr(i) for i in password])
 
 
-def Generate256ByteKeyFromPlaintext(plaintext_password):
+# Generate 256Byte Key from Plaintext Key
+def RC4_KeySchedulingAlgorithm(plaintext_password):
     generated_buffer = b''
 
     tmp_buffer = bytearray()
-    for i in range(256):
+    for i in range(RC4_Keylen):
         tmp_buffer += bytes([i])
 
     z = 0
     n = 0
-    for j in range(256):
-        z = (z + ord(plaintext_password[n]) + (tmp_buffer[j])) % 256
+    for j in range(RC4_Keylen):
+        z = (z + ord(plaintext_password[n]) + (tmp_buffer[j])) % RC4_Keylen
         y = tmp_buffer[j]
         tmp_buffer[j] = tmp_buffer[z]
         tmp_buffer[z] = y
@@ -39,7 +41,7 @@ def Generate256ByteKeyFromPlaintext(plaintext_password):
     return generated_buffer
 
 
-def DecryptRemainingBytes(encrypted_arr, key_bytes):
+def RC4Decrypt(encrypted_arr, key_bytes):
     remaining_bytes = encrypted_arr[18:]
     result = ''
 
@@ -49,26 +51,26 @@ def DecryptRemainingBytes(encrypted_arr, key_bytes):
         n += 1
         y += key_bytes[n]
         z = key_bytes[n]
-        key_bytes[n] = key_bytes[y%256]
-        key_bytes[y%256] = z
+        key_bytes[n] = key_bytes[y%RC4_Keylen]
+        key_bytes[y%RC4_Keylen] = z
         #print(hex(key_bytes[(key_bytes[y%256] + key_bytes[n]) % 256]), hex(remaining_bytes[i]))
-        result += chr((key_bytes[(key_bytes[y%256] + key_bytes[n]) % 256]) ^ remaining_bytes[i])
+        result += chr((key_bytes[(key_bytes[y%RC4_Keylen] + key_bytes[n]) % RC4_Keylen]) ^ remaining_bytes[i])
 
     return result
 
 
 def main():
-    encrypted_ip_data = getEncryptedBytes("cryptowall_055A0000.bin", 0xB8, 0x89)
+    encrypted_ip_data = GetEncryptedBytes("cryptowall_055A0000.bin", 0xB8, 0x89)
     if encrypted_ip_data[0] == 0x09:
         print("[+] Extracted encrypted data from PE File\n")
 
     password = GrabPlaintextPassword(encrypted_ip_data)
     print("[+] Got plaintext key: {}\n".format(password))
 
-    key_bytes = Generate256ByteKeyFromPlaintext(password)
+    key_bytes = RC4_KeySchedulingAlgorithm(password)
     print("[+] Generated key bytes from plaintext:\n{}\n".format(key_bytes))
 
-    ip_addr = DecryptRemainingBytes(encrypted_ip_data, key_bytes)
+    ip_addr = RC4Decrypt(encrypted_ip_data, key_bytes)
     print("Decrypted data:")
     for i in range(0, len(ip_addr), 24):
         print(ip_addr[i:i+24])
